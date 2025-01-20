075984f86472324874281654c7c36e019406e103
Jenish000007 <jenish.m0708@gmail.com>
2025-01-20 09:55:50 +0530

    Initial commit2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
     return (
-        <Router basename="/brightechsoftware.com">  {/* Set the basename here */}
+        <Router > 
             <div>
                 {/* <Toolsbar /> */}
                 {/* <Navbar /> */}
